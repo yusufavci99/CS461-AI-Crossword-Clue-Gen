06b7e0700697a0afa984c3effe6aa7d322ce06ae
mertalagozlu <59175450+mertalagozlu@users.noreply.github.com>
2020-04-28 16:47:28 +0300

    Update clueGenerator.py
    "Hello World"

--- src/clueGenerator.py
+++ src/clueGenerator.py
@@ -1,2 +1,6 @@
 def createNewClue(originalClue, entry):
+    
+    # Either We can directly get data of google, wikipedia or something else with simulated annealing algorithm.
+    # Or implement a embeded algorithm that generate clues.
+    
     return originalClue
