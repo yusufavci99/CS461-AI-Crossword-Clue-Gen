ae272566ef0a8e3178c556fe7bd66edb9e2dbba0
mertalagozlu <59175450+mertalagozlu@users.noreply.github.com>
2020-04-29 23:34:57 +0300

    Update clueGenerator.py

--- src/clueGenerator.py
+++ src/clueGenerator.py
@@ -1,6 +1,19 @@
 def createNewClue(originalClue, entry):
     
     # Either We can directly get data of google, wikipedia or something else with simulated annealing algorithm.
-    # Or implement a embeded algorithm that generate clues.
+    # Or implement a embeded algorithm that generate clues, which is very hard thing to do.
+    # Rather use the following methods, 
+    
+    #Meriam-Webster website,Oxford's dictionary can be used.
+    
+    # Couple things to be careful about.
+    #Misspelled Word Scenario (In NYT puzzle, words are not always real ones. Return failure when this is the case)
+    #Altered Entry Scenario( In webster dictionary, entries might be corrected or altered. Return failure when this is the case)
+    #Plural/Singular Form Scenario (Plural entries are corrected by webster dictionary, indicate plurality to the originalClue when this is the case)
+    #Abbreviation Scenario
+    #Foreign Word Scenario
+    #Proper Scenario: If all these conditions are successfully met, we announce success.
+    #Wordnet can be used as well with the same conditions.
+    
     
     return originalClue
